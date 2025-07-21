4237d0bab2cf3e2d6107d5b47ac3bab98f3b4785
uzma-sayyed-1104 <sayyeduzma313@gmail.com>
2025-07-21 20:43:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,27 +19,6 @@ mongoose.connect(URL)
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.log("Error connecting to MongoDB:", error));
 
-
-//defining routes
-app.use("/book", bookRoute);
-app.use("/user", userRoute)
-
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-// Required for __dirname in ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Serve static files from the React frontend
-app.use(express.static(path.join(__dirname, "./frontend/dist")));
-
-// Catch-all route to handle client-side routing
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./frontend/dist/index.html"));
-});
-
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
