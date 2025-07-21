0b5bd6bcc72a32686ebe91e87b9d33c0d314368a
uzma-sayyed-1104 <sayyeduzma313@gmail.com>
2025-07-21 19:34:56 +0530

    Update Freebook.jsx

--- frontend/src/components/Freebook.jsx
+++ frontend/src/components/Freebook.jsx
@@ -13,7 +13,7 @@ function Freebook() {
     useEffect(() => {
         const getBook = async () => {
             try {
-                const res = await axios.get("http://localhost:4001/book");
+                const res = await axios.get("https://bookstoreapp-backend-djr2.onrender.com/book");
                 
                 const data = res.data.filter((data) => data.category === "Free");
                 console.log(data);
